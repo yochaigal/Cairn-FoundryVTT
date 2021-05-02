767d2822c6e5064134bb88979e89b6038b3bb1c5
Stephen Mariano Cabrera <stephen@userinterviews.com>
2021-05-02 13:37:33 +0200

    Add standard and lint existing files

--- module/actor/actor.js
+++ module/actor/actor.js
@@ -3,62 +3,61 @@
  * @extends {Actor}
  */
 export class CairnActor extends Actor {
-    /**
+  /**
      * Augment the basic actor data with additional dynamic data.
      */
-    prepareData () {
-        super.prepareData();
+  prepareData () {
+    super.prepareData()
 
-        const actorData = this.data;
-        const data = actorData.data;
-        const flags = actorData.flags;
+    const actorData = this.data
+    const data = actorData.data
+    const flags = actorData.flags
 
-        // Make separate methods for each Actor type (character, npc, etc.) to keep
-        // things organized.
-        if (actorData.type === 'character') this._prepareCharacterData(actorData);
-    }
+    // Make separate methods for each Actor type (character, npc, etc.) to keep
+    // things organized.
+    if (actorData.type === 'character') this._prepareCharacterData(actorData)
+  }
 
-    /**
+  /**
      * Prepare Character type specific data
      */
-    _prepareCharacterData (actorData) {
-        const data = actorData.data;
+  _prepareCharacterData (actorData) {
+    const data = actorData.data
 
-        data.armor = actorData
-            .items
-            .map(item => item.data.armor * item.data.equipped)
-            .reduce((a, b) => a + b, 0)
+    data.armor = actorData
+      .items
+      .map(item => item.data.armor * item.data.equipped)
+      .reduce((a, b) => a + b, 0)
 
-        data.slotsUsed = actorData
-            .items
-            .map(item => item.data.slots * 1)
-            .reduce((memo, slots) => memo + slots, 0)
+    data.slotsUsed = actorData
+      .items
+      .map(item => item.data.slots * 1)
+      .reduce((memo, slots) => memo + slots, 0)
 
-        data.encumbered = data.slotsUsed >= 10
+    data.encumbered = data.slotsUsed >= 10
 
-        if (data.encumbered) {
-            data.hp.value = 0;
-        }
+    if (data.encumbered) {
+      data.hp.value = 0
     }
+  }
 
-
-    /** @override */
-    getRollData () {
-        const data = super.getRollData();
-        // Let us do @str etc, instead of @abilities.str.value
-        for (let [k, v] of Object.entries(data.abilities)) {
-            if (!(k in data)) data[k] = v.value;
-        }
-        return data
+  /** @override */
+  getRollData () {
+    const data = super.getRollData()
+    // Let us do @str etc, instead of @abilities.str.value
+    for (const [k, v] of Object.entries(data.abilities)) {
+      if (!(k in data)) data[k] = v.value
     }
+    return data
+  }
 
-    /** @override */
-    deleteOwnedItem (itemId) {
-        const item = this.getOwnedItem(itemId);
-        if (item.data.data.quantity > 1) {
-            item.data.data.quantity--;
-        } else {
-            super.deleteOwnedItem(itemId);
-        }
+  /** @override */
+  deleteOwnedItem (itemId) {
+    const item = this.getOwnedItem(itemId)
+    if (item.data.data.quantity > 1) {
+      item.data.data.quantity--
+    } else {
+      super.deleteOwnedItem(itemId)
     }
+  }
 }
--- module/item/item.js
+++ module/item/item.js
@@ -3,17 +3,17 @@
  * @extends {Item}
  */
 export class CairnItem extends Item {
-    /**
+  /**
      * Augment the basic Item data model with additional dynamic data.
      */
-    prepareData () {
-        super.prepareData();
+  prepareData () {
+    super.prepareData()
 
-        // Get the Item's data
-        const itemData = this.data;
-        const actorData = this.actor
-            ? this.actor.data
-            : {};
-        const data = itemData.data;
-    }
+    // Get the Item's data
+    const itemData = this.data
+    const actorData = this.actor
+      ? this.actor.data
+      : {}
+    const data = itemData.data
+  }
 }
