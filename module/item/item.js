c3296ca731c5172d676557b6ed7d4cdb78b5a29e
Oskar Świda <oskar.swida@gmail.com>
2024-12-22 11:56:33 +0100

    fix: available uses visualization

--- module/item/item.js
+++ module/item/item.js
@@ -3,14 +3,18 @@
  * @extends {Item}
  */
 export class CairnItem extends Item {
-	/**
-	 * Augment the basic item data with additional dynamic data.
-	 */
-	prepareData() {
-		super.prepareData();
-		// Items in containers cannot be equippable.
-		const actorType = this.actor ? this.actor.type : "";
-		this.system.isEquipable = ["weapon", "armor", "spellbook"].includes(this.type) && actorType != 'container';
-		this.system.hasPlusMinus = (this.system.uses?.max ?? 0) > 0;
-	}
+  /**
+   * Augment the basic item data with additional dynamic data.
+   */
+  prepareData() {
+    super.prepareData();
+    // Items in containers cannot be equippable.
+    const actorType = this.actor ? this.actor.type : "";
+    this.system.isEquipable =
+      ["weapon", "armor", "spellbook"].includes(this.type) &&
+      actorType != "container";
+    this.system.hasPlusMinus = (this.system.uses?.max ?? 0) > 0;
+    if (this.system.uses.value > this.system.uses.max)
+      this.system.uses.value = this.system.uses.max;
+  }
 }
